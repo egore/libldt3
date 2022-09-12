e5bcaf1f2b6a02933e09054f378928b8d8cf0ca5
Christoph Brill <opensource@christophbrill.de>
2022-09-12 21:01:37 +0200

    Add mock K005

---
+++ src/main/java/libldt3/model/regel/kontext/K005.java
@@ -0,0 +1,10 @@
+package libldt3.model.regel.kontext;
+
+public class K005 implements Kontextregel {
+
+	@Override
+	public boolean isValid(Object owner) throws IllegalAccessException {
+		throw new UnsupportedOperationException();
+	}
+
+}
