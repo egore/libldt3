b019320a52ed877d6ce14a605e270ed7e57300a4
Christoph Brill <opensource@christophbrill.de>
2023-06-21 10:19:08 +0200

    feat: Improve object and field name generation

--- parser/src/main/java/libldt3/parser/generation/Normalizer.java
+++ parser/src/main/java/libldt3/parser/generation/Normalizer.java
@@ -1,6 +1,11 @@
 package libldt3.parser.generation;
 
+import java.util.regex.Pattern;
+
 public class Normalizer {
+
+    private static final Pattern PATTERN_LOW_END_LOW_START = Pattern.compile("([a-z])[/ ]+([a-z])");
+    private static final Pattern PATTERN_UPPER = Pattern.compile("([A-Z]+)");
 
     private static String cleanup(String value) {
         return value
@@ -55,14 +60,18 @@ public class Normalizer {
     }
 
     public static String getObjektName(String value) {
-        return cleanup(value)
+        String replace = cleanup(value)
 
                 // Preparation: Ensure word separation is done by spaces
                 .replace("_", " ")
 
                 // Rule: When a word ends in lowercase and is followed by a word starting in uppercase, combine them
-                .replaceAll("([a-z])[/ ]+([a-zA-Z])", "$1$2")
+                .replaceAll("([a-z])[/ ]+([A-Z])", "$1$2");
 
+        // Rule: When a word ends in lowercase and is followed by a word starting in lowercase, combine them
+        replace = PATTERN_LOW_END_LOW_START.matcher(replace).replaceAll(match -> match.group(1) + match.group(2).toUpperCase());
+
+        replace = replace
                 // Rule: everything in parentheses is removed
                 .replaceAll("\\([^)]+\\)", "")
 
@@ -74,22 +83,36 @@ public class Normalizer {
                 .replace("-/", "_")
                 .replace("-", "")
                 .replace(" ", "");
+
+        // Workaround for ugly naming
+        if ("RgEmpfaenger".equals(replace)) {
+            return "Rechnungsempfaenger";
+        }
+
+        return replace;
     }
 
     public static String getFieldName(String value) {
 
+        value = value.replaceAll("^ID (.*)$", "$1Id");
 
-        if (value.matches("^[A-Z]+$")) {
-            value = value.toLowerCase();
-        } else {
-            value = value.replaceAll("^ID (.*)$", "$1Id");
-            value = value.substring(0, 1).toLowerCase() + value.substring(1);
-        }
+        // Rule: Words in uppercase only will be converted to camel case
+        value = PATTERN_UPPER.matcher(value).replaceAll(match -> toUppercaseFirst(match.group(1)));
 
-        value = value
-                .replaceAll(" des ([A-Za-z]+)s", "$1");
+        value = value.replaceAll(" des ([A-Za-z]+)s", "$1");
 
-        return getObjektName(value);
+        value = value.replace(" der ", " ");
+        value = value.replace(" im ", " ");
+
+        value = getObjektName(value);
+
+        value = value.substring(0, 1).toLowerCase() + value.substring(1);
+
+        return value;
+    }
+
+    public static String toUppercaseFirst(String value) {
+        return value.substring(0, 1).toUpperCase() + value.substring(1).toLowerCase();
     }
 
 }
