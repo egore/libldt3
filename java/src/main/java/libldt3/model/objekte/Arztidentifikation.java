a9b2bbe9195d77c997acd4fab7a0fed2bcc4e34e
Christoph Brill <opensource@christophbrill.de>
2023-06-21 14:24:54 +0200

    chore: Sync to generated code

--- java/src/main/java/libldt3/model/objekte/Arztidentifikation.java
+++ java/src/main/java/libldt3/model/objekte/Arztidentifikation.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2022  Christoph Brill <opensource@christophbrill.de>
+ * Copyright 2016-2023  Christoph Brill <opensource@christophbrill.de>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -32,37 +32,40 @@ import libldt3.model.enums.ArztTypId;
 import libldt3.model.regel.format.F011;
 import libldt3.model.regel.format.F014;
 import libldt3.model.regel.format.F022;
+import libldt3.model.regel.kontext.K057;
+import libldt3.model.regel.kontext.K115;
+import libldt3.model.regel.kontext.K116;
 
 /**
  * Hier werden alle notwendigen Informationen zum Einsender zusammengefasst.
  */
-@Objekt(value = "0014")
+@Objekt(value = "0014", kontextregeln = {K057.class, K115.class, K116.class})
 public class Arztidentifikation implements Kontext {
 
     @Objekt
-    public static class ArztId implements Kontext {
+    public static class ArztIdEinesArztes implements Kontext {
         @SuppressWarnings("unused")
         public String value;
         @Feld(value = "0308", feldart = Feldart.bedingt_muss)
         @Regelsatz(laenge = 1)
-        public ArztTypId arztTypId;
+        public List<ArztTypId> typArztId;
     }
 
-    @Feld(value = "8147", feldart = Feldart.muss)
+    @Feld(value = "8147", feldart = Feldart.bedingt_muss)
     @Regelsatz(laenge = 6)
     public Person person;
     @Feld(value = "0212", feldart = Feldart.bedingt_muss)
-    @Regelsatz(value = { F011.class, F022.class }, laenge = 9)
-    public List<String> lanr;
+    @Regelsatz(value = F011.class, laenge = 9)
+    public List<String> lebenslangeArztnummer;
     @Feld(value = "0223", feldart = Feldart.bedingt_muss)
-    @Regelsatz(value = { F011.class, F022.class }, laenge = 9)
-    public List<String> pseudoLanr;
+    @Regelsatz(value = F022.class, laenge = 9)
+    public List<String> pseudoLanrFuerKrankenhausaerzteRahmenAsvAbrechnung;
     @Feld(value = "0306", feldart = Feldart.kann)
     @Regelsatz(maxLaenge = 60)
-    public String vertragsId;
+    public String vertragsIdDesBehandelndenArztes;
     @Feld(value = "0307", feldart = Feldart.kann)
     @Regelsatz(maxLaenge = 60)
-    public List<ArztId> arztIds;
+    public List<ArztIdEinesArztes> arztIdEinesArztes;
     @Feld(value = "0222", feldart = Feldart.bedingt_muss)
     @Regelsatz(value = F014.class, laenge = 9)
     public String asvTeamnummer;
---
+++ java/src/main/java/libldt3/model/regel/kontext/K116.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2016-2023  Christoph Brill <opensource@christophbrill.de>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package libldt3.model.regel.kontext;
+
+import libldt3.model.Kontext;
+import libldt3.model.enums.Scheinuntergruppe;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Field;
+import java.util.Map;
+import java.util.Set;
+
+import static libldt3.model.regel.kontext.KontextregelHelper.containsAnyString;
+import static libldt3.model.regel.kontext.KontextregelHelper.findFields;
+import static libldt3.model.regel.kontext.KontextregelHelper.getFieldValue;
+
+/**
+ * Wenn Feldinhalt von FK 4239 ≠ 28 und wenn FK 0222 vorhanden ist, dann muss
+ * entweder FK 0212 oder FK 0223 vorhanden sein. Wenn Feldinhalt von FK 4239 = 28
+ * und wenn FK 0222 vorhanden ist, dann muss ein FK 0212 vorhanden sein. Die FK
+ * 0223 darf nicht vorhanden sein.
+ *
+ * Es ist ausgeschlossen, dass ein Krankenhausarzt im Rahmen seiner
+ * ASV-Berechtigung Mitglied einer Laborgemeinschaft ist und in diesem
+ * Zusammenhang Laborleistungen auf Muster 10A anfordert, gemäß § 25
+ * Abs. 3 S. 7 BMV-Ä.
+ */
+public class K116 implements Kontextregel {
+
+    private static final Logger LOG = LoggerFactory.getLogger(K116.class);
+
+    private static final Set<String> FIELDTYPES = Set.of("4239", "0222", "0212", "0223");
+
+    @Override
+    public boolean isValid(Kontext owner) throws IllegalAccessException {
+
+        Map<String, Field> fields = findFields(owner, FIELDTYPES);
+        if (fields.size() != FIELDTYPES.size()) {
+            LOG.error("Class of {} must have fields {}", owner, FIELDTYPES);
+            return false;
+        }
+
+        // FIXME: This field does not exist here and likely must be inherited from the context
+        Scheinuntergruppe scheinuntergruppe = (Scheinuntergruppe) getFieldValue(fields.get("4239"), owner);
+        if (scheinuntergruppe != Scheinuntergruppe.Muster10A && containsAnyString(fields.get("0222"), owner)) {
+            if (!containsAnyString(fields.get("0212"), owner) && !containsAnyString(fields.get("0223"), owner)) {
+                LOG.error("For other than Muster 10A either FK 0212 or 0223 must be present");
+                return false;
+            }
+        }
+        if (scheinuntergruppe == Scheinuntergruppe.Muster10A && containsAnyString(fields.get("0222"), owner)) {
+            if (!containsAnyString(fields.get("0212"), owner)) {
+                LOG.error("For Muster 10A FK 0212 must be present");
+                return false;
+            }
+            if (containsAnyString(fields.get("0223"), owner)) {
+                LOG.error("For Muster 10A FK 0223 must not be present");
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+}
